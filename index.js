fbf27e5671823c114c077ebbbf652446d22eb51f
Catherine Cordova <ccordova41@gmail.com>
2017-11-22 05:11:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,10 +6,3 @@ fetch('https://api.github.com/user/repos', {
     Authorization: `token ${token}`
   }
 }).then(res => res.json()).then(json => console.log(json));
-fetch('https://api.github.com/user/repos').
-  then(res => res.json()).
-  then(json => console.log(json))
-
-fetch('https://api.github.com/repos/jquery/jquery/commits')
-  .then(res => res.json())
-  .then(json => console.log(json));
