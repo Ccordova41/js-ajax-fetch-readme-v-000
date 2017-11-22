34bec3fde7c1b59ce1cc391a0624b12471b69909
Catherine Cordova <ccordova41@gmail.com>
2017-11-22 05:11:55 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 const app = "I don't do much.";
+
+const token = '0adeed0af49a7b42e1cde6a67f91f8d5be5340f7'
+fetch('https://api.github.com/user/repos', {
+  headers: {
+    Authorization: `token ${token}`
+  }
+}).then(res => res.json()).then(json => console.log(json));
