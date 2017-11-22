5764c2cc20ce778f7a6d69efd668f3aeddcb5904
Catherine Cordova <ccordova41@gmail.com>
2017-11-22 05:10:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 const app = "I don't do much.";
 
+const token = '0adeed0af49a7b42e1cde6a67f91f8d5be5340f7'
+fetch('https://api.github.com/user/repos', {
+  headers: {
+    Authorization: `token ${token}`
+  }
+}).then(res => res.json()).then(json => console.log(json));
+fetch('https://api.github.com/user/repos').
+  then(res => res.json()).
+  then(json => console.log(json))
+
 fetch('https://api.github.com/repos/jquery/jquery/commits')
   .then(res => res.json())
   .then(json => console.log(json));
